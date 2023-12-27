e808bb4c27943ea26b05e6bb338f275d097c413d
JohnnySo <27062459+JohnnySo@users.noreply.github.com>
2023-12-27 09:13:28 +0100

    feat: update kotlin version and add mapstruct dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,11 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "3.2.0"
+	id("org.springframework.boot") version "3.2.1"
 	id("io.spring.dependency-management") version "1.1.4"
-	kotlin("jvm") version "1.9.20"
-	kotlin("plugin.spring") version "1.9.20"
+	kotlin("jvm") version "1.9.22"
+	kotlin("plugin.spring") version "1.9.22"
+	kotlin("kapt") version "1.9.22"
 }
 
 group = "org.soneira"
@@ -19,11 +20,12 @@ repositories {
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
-	implementation("org.springframework.boot:spring-boot-starter-data-rest")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	implementation("org.springframework.boot:spring-boot-starter-data-mongodb:3.2.1")
+	implementation("org.springframework.boot:spring-boot-starter-data-rest:3.2.1")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.15.3")
+	implementation("org.jetbrains.kotlin:kotlin-reflect:1.9.22")
+	implementation("org.mapstruct:mapstruct:1.5.5.Final")
+	kapt("org.mapstruct:mapstruct-processor:1.5.5.Final")
 }
 
 tasks.withType<KotlinCompile> {
